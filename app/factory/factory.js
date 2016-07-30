653d110095f5683fab582e621655a3628e46873b
spainavi <spain_navi@hotmail.com>
2016-07-29 19:40:55 -0500

    app

---
+++ app/factory/factory.js
@@ -0,0 +1,12 @@
+(function(){
+	'use strict'
+
+	angular
+		.module('spotify')
+		.factory('spotifySearch',spotifySearch);
+
+	function spotifySearch($resource) {
+		return $resource('https://api.spotify.com/v1/search?type=track&q=:song&market=MX')
+
+	}
+})();
